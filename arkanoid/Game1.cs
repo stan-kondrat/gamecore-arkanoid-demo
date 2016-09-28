82b0802dc543dce27712278ad158a5d5946f2b29
Stan Kondrat <stan.kondrat@gmail.com>
2016-09-28 22:51:18 +0300

    add static ball and platform

--- arkanoid/Game1.cs
+++ arkanoid/Game1.cs
@@ -14,16 +14,45 @@ namespace arkanoid
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+		const int VIEWPORT_WIDTH = 640;
+		const int VIEWPORT_HEIGHT = 480;
+		const int PLATFORM_PADDING = 10;
+		const int PLATFORM_HEIGHT = 10;
+
         private Texture2D background;
 
         private const float timeToNextUpdate = 1.0f / 30.0f;
         private float timeSinceLastUpdate;
 
-        public Game1()
+		public struct GameObject
+		{
+			public Texture2D texture;
+			public int x;
+			public int y;
+			public int width;
+			public int height;
+		}
+
+		private GameObject platform;
+		private GameObject ball;
+
+		public Game1() 
         {
             graphics = new GraphicsDeviceManager(this);
+			graphics.PreferredBackBufferHeight = VIEWPORT_HEIGHT;
+			graphics.PreferredBackBufferWidth = VIEWPORT_WIDTH;
 
             Content.RootDirectory = "Content";
+
+			platform.width = VIEWPORT_WIDTH / 10;
+			platform.height = PLATFORM_HEIGHT;
+			platform.x = (VIEWPORT_WIDTH - platform.width) / 2;
+			platform.y = VIEWPORT_HEIGHT - PLATFORM_PADDING - platform.height;
+
+			ball.width = 20;
+			ball.height = 20;
+			ball.x = (VIEWPORT_WIDTH - ball.width) / 2;
+			ball.y = VIEWPORT_HEIGHT - PLATFORM_PADDING - platform.height - ball.height;
         }
 
         /// <summary>
@@ -46,7 +75,9 @@ namespace arkanoid
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            background = this.Content.Load<Texture2D>("blank");
+            background = this.Content.Load<Texture2D>("background");
+			platform.texture = this.Content.Load<Texture2D>("blank");
+			ball.texture = this.Content.Load<Texture2D>("ball");
         }
 
         /// <summary>
@@ -83,8 +114,26 @@ namespace arkanoid
         {
             graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            spriteBatch.Begin();
-            spriteBatch.Draw(background, new Rectangle(0, 0, 800, 480), Color.White);
+			spriteBatch.Begin(SpriteSortMode.Immediate);
+
+            spriteBatch.Draw(
+				background,
+				new Rectangle(0, 0, VIEWPORT_WIDTH, VIEWPORT_HEIGHT),
+				Color.White
+			);
+
+			spriteBatch.Draw(
+				platform.texture, 
+				new Rectangle(platform.x, platform.y, platform.width, platform.height), 
+				Color.White
+			);
+
+			spriteBatch.Draw(
+				ball.texture,
+				new Rectangle(ball.x, ball.y, ball.width, ball.height),
+				Color.White
+			);
+
             spriteBatch.End();
 
             base.Draw(gameTime);
